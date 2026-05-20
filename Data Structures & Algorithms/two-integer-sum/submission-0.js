6c53a2ca64ea8ac6d9444db367fdde56504527bf
Sandyk0000 <52044350+Sandyk0000@users.noreply.github.com>
2026-05-21 01:19:18 +0530

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        let map = new Map();
+        for(let i=0;i<nums.length;i++){
+            let req = target - nums[i];
+            if(map.has(req)){
+                return [map.get(req), i]
+            }
+            map.set(nums[i], i)
+        }
+    }
+}
